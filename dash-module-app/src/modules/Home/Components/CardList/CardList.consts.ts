b3f7597abb27ab90f6ed25a11cbde03001ba3a52
LucasBert432 <lucasroberto432@gmail.com>
2025-02-21 04:39:05 -0300

    feat: create components cardlist

---
+++ dash-module-app/src/modules/Home/Components/CardList/CardList.consts.ts
@@ -0,0 +1,12 @@
+export const initialCards = [
+  {
+    image: "https://via.placeholder.com/300",
+    title: "Card 1",
+    description: "Descrição do Card 1",
+  },
+  {
+    image: "https://via.placeholder.com/300",
+    title: "Card 2",
+    description: "Descrição do Card 2",
+  },
+];
