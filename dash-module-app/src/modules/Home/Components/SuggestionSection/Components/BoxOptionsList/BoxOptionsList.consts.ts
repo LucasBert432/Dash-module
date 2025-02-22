587065d02516aad143fdf8e93d830d6b4038e15c
LucasBert432 <lucasroberto432@gmail.com>
2025-02-22 13:42:16 -0300

    feat: create component SuggestionSection

---
+++ dash-module-app/src/modules/Home/Components/SuggestionSection/Components/BoxOptionsList/BoxOptionsList.consts.ts
@@ -0,0 +1,20 @@
+export const initialBoxOptions = [
+  {
+    text: "Eventos de hoje",
+  },
+  {
+    text: "Eventos de amanhã",
+  },
+  {
+    text: "Eventos deste fim de semana",
+  },
+  {
+    text: "Eventos do fim de semana seguinte",
+  },
+  {
+    text: "Eventos deste mês",
+  },
+  {
+    text: "Eventos do mês seguinte",
+  },
+];
