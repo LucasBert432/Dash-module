f8ab2e5d436ec72113aba0e223bd2026debf49af
LucasBert432 <lucasroberto432@gmail.com>
2025-02-22 13:41:55 -0300

    feat: create component SectionHome

---
+++ dash-module-app/src/modules/Home/Components/SectionHome/styles.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+const useStyles = () => {
+  return {
+    wrapper: css`
+      margin-top: 72px;
+    `,
+    wrapperCircule: css`
+      margin-right: 24px;
+    `,
+  };
+};
+
+export default useStyles;
