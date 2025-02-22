1ede41cb1358f5def86fd5bc91ceb0f791cc138e
LucasBert432 <lucasroberto432@gmail.com>
2025-02-22 13:40:06 -0300

    fix: revert logic global style

--- dash-module-app/src/globalStore/globalStore.ts
+++ dash-module-app/src/globalStore/globalStore.ts
@@ -2,6 +2,6 @@ import { create } from "zustand";
 import { ThemeState } from "./globalStore.types";
 
 export const useThemeStore = create<ThemeState>((set) => ({
-  isWhite: false,
+  isWhite: true,
   toggleTheme: () => set((state) => ({ isWhite: !state.isWhite })),
 }));
